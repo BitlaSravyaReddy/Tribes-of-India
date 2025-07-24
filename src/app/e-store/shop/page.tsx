ca127923fb1bc268e9ea182667a6f825a7f44038
BitlaSravyaReddy <bitlashravyareddy@gmail.com>
2025-07-25 01:06:00 +0530

    added shop page dynamic

--- src/app/e-store/shop/page.tsx
+++ src/app/e-store/shop/page.tsx
@@ -1,4 +1,5 @@
 // app/e-store/shop/page.tsx
+export const dynamic = "force-dynamic";
 
 import { ShopClient } from "@/components/e-store/shop-page-client";
 import { products, categories } from "@/lib/e-store/data";
